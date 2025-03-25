06777a55c77f506d743c28b8eaab90368fbc5858
dev <wrongcode@proton.me>
2025-03-26 01:15:57 +0300

    windows shell changes

---
+++ windows/shell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,34 @@
+# Initialize Oh My Posh
+& ([ScriptBlock]::Create((oh-my-posh init pwsh --config "$env:USERPROFILE\.dotfiles\windows\shell\oh-my-posh\uew.omp.json" --print) -join "`n"))
+$env:__SuppressProfilesLoadingMessage = $true
+
+# Set the KOMOREBI_CONFIG_HOME environment variable
+$Env:KOMOREBI_CONFIG_HOME = "$($Env:USERPROFILE)"
+
+# Import the Chocolatey Profile for tab-completion support
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+    Import-Module "$ChocolateyProfile"
+}
+
+Set-PSReadLineOption -BellStyle None
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -Colors @{ InlinePrediction = "Green" }
+
+function ff {
+    Push-Location "$env:USERPROFILE\.dotfiles\windows\shell\fastfetch"
+    fastfetch -c "default.jsonc"
+    Pop-Location
+}
+
+function y {
+    $tmp = [System.IO.Path]::GetTempFileName()
+    yazi $args --cwd-file="$tmp"
+    $cwd = Get-Content -Path $tmp -Encoding UTF8
+    if (-not [String]::IsNullOrEmpty($cwd) -and $cwd -ne $PWD.Path) {
+        Set-Location -LiteralPath ([System.IO.Path]::GetFullPath($cwd))
+    }
+    Remove-Item -Path $tmp
+}
+
+clear
