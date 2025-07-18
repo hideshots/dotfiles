3be00d3ffc07f03a98b8c5b172d7a06fe1260dcb
wsl <wsl@nixos.com>
2025-07-18 21:32:14 +0000

    chore(nixvim/obsidian): update obsidian.nix

--- nixos/hosts/wsl/home.nix
+++ nixos/hosts/wsl/home.nix
@@ -36,6 +36,9 @@
   };
 
   home.packages = with pkgs; [
+    ctop
+    ternimal
+    clang
   ];
 
   home = {
--- nixos/modules/user/nixvim/plugins/obsidian.nix
+++ nixos/modules/user/nixvim/plugins/obsidian.nix
@@ -9,9 +9,9 @@ let
 in {
   programs.nixvim = {
     plugins.obsidian = {
-      enable   = true;
-      autoLoad = true;
+      enable = true;
       settings.workspaces = obsWorkspaces;
+      settings.legacy_commands = false;
       settings = {
         note_id_func = ''
           function(title)
@@ -24,8 +24,8 @@ in {
                 return tostring(os.time())
               end
             end
-              '';
-              note_path_func = ''
+        '';
+        note_path_func = ''
           function(spec)
             return (spec.dir / spec.id):with_suffix(".md")
           end
@@ -33,45 +33,45 @@ in {
       };
     };
 
-    keymaps = [
-      # Open in Obsidian app
-      { mode = "n"; key = "<leader>oo"; action = "<cmd>ObsidianOpen<CR>"; options = { desc = "Open in App"; noremap = true; silent = true; }; }
+    keymaps = [  
+      # Open in Obsidian app  
+      { mode = "n"; key = "<leader>oo"; action = "<cmd>Obsidian open<CR>"; options = { desc = "Open in App"; noremap = true; silent = true; }; }  
 
-      # New note
-      { mode = "n"; key = "<leader>on"; action = "<cmd>ObsidianNew<CR>"; options = { desc = "New Note"; noremap = true; silent = true; }; }
+      # New note  
+      { mode = "n"; key = "<leader>on"; action = "<cmd>Obsidian new<CR>"; options = { desc = "New Note"; noremap = true; silent = true; }; }  
 
-      # Quick switch
-      { mode = "n"; key = "<leader>oq"; action = "<cmd>ObsidianQuickSwitch<CR>"; options = { desc = "Notes List"; noremap = true; silent = true; }; }
+      # Quick switch  
+      { mode = "n"; key = "<leader>oq"; action = "<cmd>Obsidian quick_switch<CR>"; options = { desc = "Notes List"; noremap = true; silent = true; }; }  
 
-      # Follow link
-      { mode = "n"; key = "<leader>of"; action = "<cmd>ObsidianFollowLink<CR>"; options = { desc = "Follow Link"; noremap = true; silent = true; }; }
+      # Follow link  
+      { mode = "n"; key = "<leader>of"; action = "<cmd>Obsidian follow_link<CR>"; options = { desc = "Follow Link"; noremap = true; silent = true; }; }  
 
-      # Backlinks
-      { mode = "n"; key = "<leader>ob"; action = "<cmd>ObsidianBacklinks<CR>"; options = { desc = "Back Links"; noremap = true; silent = true; }; }
+      # Backlinks  
+      { mode = "n"; key = "<leader>ob"; action = "<cmd>Obsidian backlinks<CR>"; options = { desc = "Back Links"; noremap = true; silent = true; }; }  
 
-      # Tags
-      { mode = "n"; key = "<leader>ot"; action = "<cmd>ObsidianTags<CR>"; options = { desc = "Tags"; noremap = true; silent = true; }; }
+      # Tags  
+      { mode = "n"; key = "<leader>ot"; action = "<cmd>Obsidian tags<CR>"; options = { desc = "Tags"; noremap = true; silent = true; }; }  
 
-      # Daily notes
-      { mode = "n"; key = "<leader>od"; action = "<cmd>ObsidianToday<CR>"; options = { desc = "Daily"; noremap = true; silent = true; }; }
-      { mode = "n"; key = "<leader>oy"; action = "<cmd>ObsidianYesterday<CR>"; options = { desc = "Yesterday"; noremap = true; silent = true; }; }
-      { mode = "n"; key = "<leader>om"; action = "<cmd>ObsidianTomorrow<CR>"; options = { desc = "Tomorrow"; noremap = true; silent = true; }; }
-      { mode = "n"; key = "<leader>oD"; action = "<cmd>ObsidianDailies<CR>"; options = { desc = "Dailies"; noremap = true; silent = true; }; }
+      # Daily notes  
+      { mode = "n"; key = "<leader>od"; action = "<cmd>Obsidian today<CR>"; options = { desc = "Daily"; noremap = true; silent = true; }; }  
+      { mode = "n"; key = "<leader>oy"; action = "<cmd>Obsidian yesterday<CR>"; options = { desc = "Yesterday"; noremap = true; silent = true; }; }  
+      { mode = "n"; key = "<leader>om"; action = "<cmd>Obsidian tomorrow<CR>"; options = { desc = "Tomorrow"; noremap = true; silent = true; }; }  
+      { mode = "n"; key = "<leader>oD"; action = "<cmd>Obsidian dailies<CR>"; options = { desc = "Dailies"; noremap = true; silent = true; }; }  
 
-      # Linking
-      { mode = "v"; key = "<leader>ol"; action = "<cmd>ObsidianLink<CR>"; options = { desc = "Link"; noremap = true; silent = true; }; }
-      { mode = "v"; key = "<leader>oL"; action = "<cmd>ObsidianLinkNew<CR>"; options = { desc = "New Link"; noremap = true; silent = true; }; }
+      # Linking  
+      { mode = "v"; key = "<leader>ol"; action = "<cmd>Obsidian link<CR>"; options = { desc = "Link"; noremap = true; silent = true; }; }  
+      { mode = "v"; key = "<leader>oL"; action = "<cmd>Obsidian link_new<CR>"; options = { desc = "New Link"; noremap = true; silent = true; }; }  
 
-      # Paste images & rename
-      { mode = "n"; key = "<leader>oP"; action = "<cmd>ObsidianPasteImg<CR>"; options = { desc = "Paste Image"; noremap = true; silent = true; }; }
-      { mode = "n"; key = "<leader>or"; action = "<cmd>ObsidianRename<CR>"; options = { desc = "Rename"; noremap = true; silent = true; }; }
+      # Paste images & rename  
+      { mode = "n"; key = "<leader>oP"; action = "<cmd>Obsidian paste_img<CR>"; options = { desc = "Paste Image"; noremap = true; silent = true; }; }  
+      { mode = "n"; key = "<leader>or"; action = "<cmd>Obsidian rename<CR>"; options = { desc = "Rename"; noremap = true; silent = true; }; }  
 
-      # Extract & workspaces
-      { mode = "v"; key = "<leader>ox"; action = "<cmd>ObsidianExtractNote<CR>"; options = { desc = "Extract Note"; noremap = true; silent = true; }; }
-      { mode = "n"; key = "<leader>ow"; action = "<cmd>ObsidianWorkspace<CR>"; options = { desc = "Switch Workspace"; noremap = true; silent = true; }; }
+      # Extract & workspaces  
+      { mode = "v"; key = "<leader>ox"; action = "<cmd>Obsidian extract_note<CR>"; options = { desc = "Extract Note"; noremap = true; silent = true; }; }  
+      { mode = "n"; key = "<leader>ow"; action = "<cmd>Obsidian workspace<CR>"; options = { desc = "Switch Workspace"; noremap = true; silent = true; }; }  
 
-      # Table of Contents
-      { mode = "n"; key = "<leader>oc"; action = "<cmd>ObsidianTOC<CR>"; options = { desc = "Table of Contents"; noremap = true; silent = true; }; }
+      # Table of Contents  
+      { mode = "n"; key = "<leader>oc"; action = "<cmd>Obsidian toc<CR>"; options = { desc = "Table of Contents"; noremap = true; silent = true; }; }  
     ];
   };
 
